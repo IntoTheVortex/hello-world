700ea98ef1b498eabb29d02d070e705cc7d042cb
IntoTheVortex <amberadelaideshore@gmail.com>
2020-10-14 16:13:50 -0700

    added main method to print hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,14 @@
 package edu.pcc.cis.helloworld;
 
- 
+/*
+@author Amber Shore
+@version 2020.10.14
+Description:
+Hello World project to test usage of Git and Github.
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
